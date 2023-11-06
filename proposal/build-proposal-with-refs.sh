13e9a0f5c1b6bcc7d7bb595074712d23fe6c5b8e
Alexander Van Hecke <alexander.vanhecke@student.ou.nl>
2023-11-06 08:44:48 +0100

    remove old proposal folder

--- proposal/build-proposal-with-refs.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euv
-
-docker run --rm -v "$PWD":/data mrchoke/texlive bash -c "cd data && pdflatex proposal.tex && bibtex proposal && pdflatex proposal.tex && pdflatex proposal.tex"
