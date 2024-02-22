58674a4d769015fe13b3963ab1806020ad1e5498
Alexander Van Hecke <alexander@m1mac.local>
2024-02-22 09:41:21 +0100

    verwijderen oude bestanden

--- report/build-report-with-refs.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euv
-
-docker run --rm -v "$PWD":/data mrchoke/texlive bash -c "cd data && pdflatex report.tex && bibtex report && pdflatex report.tex && pdflatex report.tex"
--- report/build-report.sh
+++ report/build-report.sh
@@ -1,4 +1,1 @@
-#!/usr/bin/env bash
-set -euv
-
-docker run --rm -v "$PWD":/data mrchoke/texlive bash -c "cd data && pdflatex report.tex"
+rm report.pdf;docker run --rm -v "$PWD":/data mrchoke/texlive bash -c "cd data && pdflatex report.tex && biber report && pdflatex report.tex && pdflatex report.tex";open report.pdf
